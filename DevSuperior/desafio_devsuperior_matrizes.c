7a6c7bc9fed8d24128aa94eb8c1b73d3c584681c
Leandro Silva Carvalho <leocar340@gmail.com>
2024-07-16 11:17:26 -0300

    Commit Desafio_Telefone(matrizes)

--- DevSuperior/desafio_devsuperior_matrizes.c
+++ DevSuperior/desafio_devsuperior_matrizes.c
@@ -1,6 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*Fazer a leitura dos dados de um conjunto de clientes de uma companhia
+telefônica, informando, para cada um, o seu nome, o número do seu telefone,
+o tipo de assinatura (0, 1 ou 2) e o número de minutos consumidos no mês
+(não é para fazer a leitura do valor da conta, pois este será calculado
+automaticamente mais tarde). Veja o exemplo abaixo:
+
+                Nome Telefone Tipo Minutos ValorDaConta
+                Maria José 3222-1234 1 120
+                XY Informática 3223-6666 2 457
+                Joaquim Silva 3222-3344 1 50
+                Antônio Carlos 3212-6622 0 134
+
+Cada coluna da tabela deve ser armazenada em um VETOR.
+Fazer a leitura também dos três tipos de assinatura, sendo que cada
+tipo de assinatura possui um preço básico, e o preço do minuto excedente.
+Utilizar uma MATRIZ 3x2 para armazená-los, sendo que a primeira coluna
+corresponde ao preço básico da assinatura e a segunda coluna corresponde
+ao preço do minuto excedente, conforme mostrado no exemplo abaixo:
+    0   1
+0 25.5 0.10
+1 35.0 0.12
+2 60.0 0.15
+
+(certifique-se de que você entendeu a matriz acima. Por exemplo: a matriz diz,
+por exemplo, que o preço básico da assinatura tipo 1 é 35 reais e que cada
+minuto excedente deste tipo custa 12 centavos)
+
+Depois de ler os dados dos clientes, bem como os dados dos três tipos de
+assinatura, calcular o valor da conta de cada cliente e mostrar uma tabela
+conforme exemplo abaixo. A regra de cálculo da conta de um cliente é a seguinte:
+se o número de minutos consumidos for menor ou igual a 90, então o valor da conta
+é simplesmente o preço básico da assinatura; caso contrário, o valor da conta é o
+preço básico mais o valor total dos minutos excedentes a 90. Por exemplo: se um
+cliente tem uma assinatura tipo 1 e consumiu 120 minutos, repare que ele excedeu 30
+minutos além dos 90. Assim, o preço da conta dele será 35.0 + (30 * 0.12), que será
+igual a 38.60.*/
+
 int main()
 {
     int n;
